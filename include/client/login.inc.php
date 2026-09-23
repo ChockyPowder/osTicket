607bf3aba61aa8dee92346020f19b0b91c5cafc7
ChockyPowder <ChockyPowderr@proton.me>
2026-09-23 16:40:35 +0100

    Replace customer login form with Discord button

--- include/client/login.inc.php
+++ include/client/login.inc.php
@@ -1,72 +1,30 @@
 <?php
-if(!defined('OSTCLIENTINC')) die('Access Denied');
-
-$email=Format::input($_POST['luser']?:$_GET['e']);
-$passwd=Format::input($_POST['lpasswd']?:$_GET['t']);
-
-$content = Page::lookupByType('banner-client');
-
-if ($content) {
-    list($title, $body) = $ost->replaceTemplateVariables(
-        array($content->getLocalName(), $content->getLocalBody()));
-} else {
-    $title = __('Sign In');
-    $body = __('To better serve you, we encourage our clients to register for an account and verify the email address we have on record.');
-}
-
+if (!defined('OSTCLIENTINC'))
+    die('Access Denied');
 ?>
-<h1><?php echo Format::display($title); ?></h1>
-<p><?php echo Format::display($body); ?></p>
-<form action="login.php" method="post" id="clientLogin">
-    <?php csrf_token(); ?>
-<div style="display:table-row">
-    <div class="login-box">
-    <strong><?php echo Format::htmlchars($errors['login']); ?></strong>
-    <div>
-        <input id="username" placeholder="<?php echo __('Email or Username'); ?>" type="text" name="luser" size="30" value="<?php echo $email; ?>" class="nowarn">
-    </div>
-    <div>
-        <input id="passwd" placeholder="<?php echo __('Password'); ?>" type="password" name="lpasswd" size="30" maxlength="128" value="<?php echo $passwd; ?>" class="nowarn"></td>
-    </div>
-    <p>
-        <input class="btn" type="submit" value="<?php echo __('Sign In'); ?>">
-<?php if ($suggest_pwreset) { ?>
-        <a style="padding-top:4px;display:inline-block;" href="pwreset.php"><?php echo __('Forgot My Password'); ?></a>
-<?php } ?>
-    </p>
-    </div>
-    <div style="display:table-cell;padding: 15px;vertical-align:top">
-<?php
+<div class="discord-login-page">
+    <h1><?php echo __('Support'); ?></h1>
+    <p><?php echo __('Sign in with Discord to create and manage support tickets.'); ?></p>
 
-$ext_bks = array();
-foreach (UserAuthenticationBackend::allRegistered() as $bk)
-    if ($bk instanceof ExternalAuthentication)
-        $ext_bks[] = $bk;
+    <?php if (!empty($errors['err'])) { ?>
+        <div id="msg_error"><?php echo Format::htmlchars($errors['err']); ?></div>
+    <?php } ?>
 
-if (count($ext_bks)) {
-    foreach ($ext_bks as $bk) { ?>
-<div class="external-auth"><?php $bk->renderExternalLink(); ?></div><?php
+    <?php
+    $bk = UserAuthenticationBackend::getBackend('discord');
+    if ($bk instanceof ExternalAuthentication) {
+        echo '<div class="external-auth">';
+        $bk->renderExternalLink();
+        echo '</div>';
     }
-}
-if ($cfg && $cfg->isClientRegistrationEnabled()) {
-    if (count($ext_bks)) echo '<hr style="width:70%"/>'; ?>
-    <div style="margin-bottom: 5px">
-    <?php echo __('Not yet registered?'); ?> <a href="account.php?do=create"><?php echo __('Create an account'); ?></a>
-    </div>
-<?php } ?>
-    <div>
-    <b><?php echo __("I'm an agent"); ?></b> —
-    <a href="<?php echo ROOT_PATH; ?>scp/"><?php echo __('sign in here'); ?></a>
-    </div>
-    </div>
+    ?>
+
+    <p class="discord-login-note">
+        <?php echo __('Only members of our Discord server can use this support site.'); ?>
+    </p>
+
+    <p>
+        <b><?php echo __("I'm an agent"); ?></b> —
+        <a href="<?php echo ROOT_PATH; ?>scp/"><?php echo __('sign in here'); ?></a>
+    </p>
 </div>
-</form>
-<br>
-<p>
-<?php
-if ($cfg->getClientRegistrationMode() != 'disabled'
-    || !$cfg->isClientLoginRequired()) {
-    echo sprintf(__('If this is your first time contacting us or you\'ve lost the ticket number, please %s open a new ticket %s'),
-        '<a href="open.php">', '</a>');
-} ?>
-</p>
